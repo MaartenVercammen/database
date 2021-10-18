67a9409bdabfb0723b8da524b25fac4241953fd3
emvee2000 <34810297+emvee2000@users.noreply.github.com>
2021-10-18 14:30:42 +0200

    improce

---
+++ src/main/java/SecreteData.java
@@ -0,0 +1,11 @@
+import java.util.Properties;
+
+public interface SecreteData {
+    /*
+    public void setLoginData(Properties properties){
+        properties.setProperty("user", "user");
+        properties.setProperty("password", "password");
+    }
+     */
+    public void setLoginData(Properties properties)
+}
